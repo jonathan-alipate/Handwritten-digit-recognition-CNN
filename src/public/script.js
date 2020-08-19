155ae9bf75bde4637c9e172fb9027fcd3dfc697a
Jonathan Alipate <j.alipate@gmail.com>
2020-08-20 00:44:22 +1200

    completed

--- src/public/script.js
+++ src/public/script.js
@@ -38,6 +38,8 @@ async function run() {
     const model = getModel();
     tfvis.show.modelSummary({ name: 'Model Architecture' }, model);
     await train(model, data);
+    await showAccuracy(model, data);
+    await showConfusion(model, data);
 }
 
 document.addEventListener('DOMContentLoaded', run);
@@ -111,8 +113,8 @@ async function train(model, data) {
     const fitCallbacks = tfvis.show.fitCallbacks(container, metrics);
 
     const BATCH_SIZE = 512;
-    const TRAIN_DATA_SIZE = 5500;
-    const TEST_DATA_SIZE = 1000;
+    const TRAIN_DATA_SIZE = 55000;
+    const TEST_DATA_SIZE = 10000;
 
     const [trainXs, trainYs] = tf.tidy(() => {
         const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
@@ -137,4 +139,38 @@ async function train(model, data) {
         shuffle: true,
         callbacks: fitCallbacks
     });
+}
+
+const classNames = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+
+function doPrediction(model, data, testDataSize = 500) {
+    const IMAGE_WIDTH = 28;
+    const IMAGE_HEIGHT = 28;
+    const testData = data.nextTestBatch(testDataSize);
+    const testxs = testData.xs.reshape([testDataSize, IMAGE_WIDTH, IMAGE_HEIGHT, 1]);
+    const labels = testData.labels.argMax(-1);
+    const preds = model.predict(testxs).argMax(-1);
+
+    testxs.dispose();
+    return [preds, labels];
+}
+
+
+async function showAccuracy(model, data) {
+    const [preds, labels] = doPrediction(model, data);
+    const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds);
+    const container = { name: 'Accuracy', tab: 'Evaluation' };
+    tfvis.show.perClassAccuracy(container, classAccuracy, classNames);
+
+    labels.dispose();
+}
+
+async function showConfusion(model, data) {
+    const [preds, labels] = doPrediction(model, data);
+    const confusionMatrix = await tfvis.metrics.confusionMatrix(labels, preds);
+    const container = { name: 'Confusion Matrix', tab: 'Evaluation' };
+    tfvis.render.confusionMatrix(
+        container, { values: confusionMatrix }, classNames);
+
+    labels.dispose();
 }
